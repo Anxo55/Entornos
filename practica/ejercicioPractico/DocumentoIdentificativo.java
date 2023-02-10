20e408bdf41d0c3e1afdd4ef9cecd7a9585f83b3
Anxo Campos <anxo.campos260204@gmail.com>
2023-02-10 12:44:53 +0100

    all

--- practica/ejercicioPractico/DocumentoIdentificativo.java
+++ practica/ejercicioPractico/DocumentoIdentificativo.java
@@ -3,93 +3,64 @@
 // Optimizarlo
 // Documentarlo
 
-/*Clase que deine un documento de tipo CIF o NIF 
+/*Clase que defsine un documento de tipo CIF o NIF 
  * @author
 */
 
-public class DocumentoIdentificativo {
-    // Puede ser CIF o NIF
-	String type;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Clase que define un documento de tipo CIF o NIF
+ * @author Angel
+ * @version 1.0
+ *
+ */
+abstract public class DocumentoIdentificativo {
+	/**
+	 * Texto del cif o nif
+	 */
 	String text;
 	
-	String getLetter() {
-		String letraEncontrada = "";
-		if(type == "CIF") {
-			char temp =  text.charAt(0);
-			letraEncontrada = String.valueOf(temp);
-		}else if(type == "NIF"){
-			char temp = text.charAt(text.length()-1);
-			letraEncontrada = String.valueOf(temp);
-		}else {
-			System.out.println("Error en el tipo de documento");
-		}
-		return letraEncontrada;
-	}
+	/**
+	 * Obtiene la letra alfabetica del documento
+	 * @return La letra
+	 */
+	abstract String getLetter();
 	
+	/**
+	 * Valida si el documento es correcto
+	 * @return un booleano indicando si es correcto
+	 */
+	abstract boolean isValid();
+	
+}
+
+class Nif extends DocumentoIdentificativo{
+	String getLetter() {
+		char temp = text.charAt(text.length()-1);
+		return String.valueOf(temp);
+	}
 	boolean isValid() {
-		
-		if(type=="CIF") {
-			boolean tieneLetra = false;
-			boolean tieneNum1 = false;
-			boolean tieneNum2 = false;
-			boolean tieneNum3 = false;
-			boolean tieneNum4 = false;
-			boolean tieneNum5 = false;
-			boolean tieneNum6 = false;
-			boolean tieneNum7 = false;
-			boolean tieneNum8 = false;
-			
-			
-			if( Character.isAlphabetic(text.charAt(0))  == true) {
-				tieneLetra = true;
-			}
-			
-			if( Character.isDigit(text.charAt(1))  == true) {
-				tieneNum1 = true;
-			}
-			
-			if( Character.isDigit(text.charAt(2))  == true) {
-				tieneNum2 = true;
-			}
-			
-			if( Character.isDigit(text.charAt(3))  == true) {
-				tieneNum3 = true;
-			}
-			
-			if( Character.isDigit(text.charAt(4))  == true) {
-				tieneNum4 = true;
-			}
-			
-			if( Character.isDigit(text.charAt(5))  == true) {
-				tieneNum5 = true;
-			}
-			
-			if( Character.isDigit(text.charAt(6))  == true) {
-				tieneNum6 = true;
-			}
-			
-			if( Character.isDigit(text.charAt(7))  == true) {
-				tieneNum7 = true;
-			}
-			if( Character.isDigit(text.charAt(8))  == true) {
-				tieneNum8 = true;
-			}
-			
-			if(tieneLetra && tieneNum1 && tieneNum2 && tieneNum3 && tieneNum4
-					&& tieneNum5 && tieneNum6 && tieneNum7 && tieneNum8) {
-				return true;
-			}else {
-				return false;
-			}
-			
-		}else if(text == "NIF") {
-			//TODO falta por hacer
-			return false;
-		}
+		//TODO falta por hacer
 		return false;
+	}
+}
+
+class Cif extends DocumentoIdentificativo{
+	String getLetter() {
+		char temp =  text.charAt(0);
+		return String.valueOf(temp);
+	}
+	boolean isValid() {
+		// empezar por letra(A-Z) y luego tener 8 numeros
+		Pattern pattern = Pattern.compile( "^[A-Z]\\d{8}$" );
+		Matcher matcher = pattern.matcher(text);
+		return matcher.matches();
 	}
 	
 }
+
 
     
 
--- practica/ejercicioPractico/DocumentoIdentificativoTest.java
+++ practica/ejercicioPractico/DocumentoIdentificativoTest.java
@@ -1,3 +1,22 @@
-public class DocumentoIdentificativoTest {
-    
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class DocumentoIdentificativoTest {
+
+	@Test
+	void testGetLetter() {
+		DocumentoIdentificativo doc = new Nif();
+		doc.text="36765546N";
+		assertEquals("N", doc.getLetter());
+	}
+
+	@Test
+	void testIsValid() {
+		DocumentoIdentificativo doc = new Cif();
+		doc.text="B34455667";
+		assertEquals(true, doc.isValid());
+		assertTrue(doc.isValid());
+	}
+
 }
